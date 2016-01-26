e910e2001e52cf1df44294a4e73020f685bf886a
App Academy Student <appacademy@Apps-Mac-mini-4.local>
2016-01-26 17:28:12 -0500

    add user index item styling.

--- frontend/components/user_item.jsx
+++ frontend/components/user_item.jsx
@@ -5,8 +5,9 @@ var UserItem = React.createClass({
 
   render: function () {
     return (
-      <div className="user-item">
-        {this.props.user.username}
+      <div className="user-item group">
+        <div className="user-profile-thumb"><img src="/assets/guest_profile_picture.jpg"/></div>
+        <h2 className="user-name">{this.props.user.username}</h2>
       </div>
     );
   }
--- frontend/jambox.jsx
+++ frontend/jambox.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var UserFeed = require('./components/user_feed');
+
 var Jambox = React.createClass({
 
   render: function () {
