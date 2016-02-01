cfb095006075e69009e6e112ef59d2960507fd0f
App Academy Student <appacademy@Apps-Mac-mini-76.local>
2016-02-01 09:11:46 -0500

    fixed follow button logic on song show page.

--- frontend/components/comments/comment_index_large_user_info.jsx
+++ frontend/components/comments/comment_index_large_user_info.jsx
@@ -1,15 +1,22 @@
 var React = require('react');
 var FollowButton = require('../buttons/follow_button');
+var CurrentUserStore = require('../../stores/current_user_store');
 
 var CommentIndexLargeUserInfo = React.createClass({
 
   render: function () {
+    var followButton;
+    if (CurrentUserStore.currentUserId() === this.props.user.id) {
+      followButton = "";
+    } else {
+      followButton = <FollowButton followedId={this.props.user.id} />;
+    }
     return (
       <div className="comment-index-large-user-info">
         <img className="thumb-med" src={this.props.user.image_url} />
         <div className="artist-info-name">{this.props.user.username}</div>
         <div className="follow-button-container">
-          <FollowButton followedId={this.props.user.id} />
+          {followButton}
         </div>
       </div>
     );
