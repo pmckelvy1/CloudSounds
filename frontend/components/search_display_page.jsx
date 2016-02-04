2985c75478263b0a21bff30943a744f58d9f2c0f
App Academy Student <appacademy@Apps-Mac-mini-55.local>
2016-02-04 16:37:38 -0500

    render search results with track player mini and track user minis.

--- frontend/components/search_display_page.jsx
+++ frontend/components/search_display_page.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var SearchResultsStore = require('../stores/search_results_store');
+var TrackUserMini = require('./audio/track_user_mini');
+var TrackPlayerMini = require('./audio/track_player_mini');
 
 var SearchDisplayPage = React.createClass({
   getInitialState: function () {
@@ -9,9 +11,13 @@ var SearchDisplayPage = React.createClass({
   render: function () {
     var searchResults = SearchResultsStore.all().map(function (searchResult) {
       if (searchResult._type === "User") {
-        return <li key={searchResult.id} className="search-result-index-item">{searchResult.username}</li>;
+        return <li key={searchResult.id} className="search-result-index-item">
+          <TrackUserMini user={searchResult} />
+        </li>;
       } else {
-        return <li key={searchResult.id} className="search-result-index-item">{searchResult.username}</li>;
+        return <li key={searchResult.id} className="search-result-index-item">
+          <TrackPlayerMini song={searchResult} />
+        </li>;
       }
     });
 
