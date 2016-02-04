a200b530a59dfaa75c8d17c1a6f17944fbb3d563
App Academy Student <appacademy@Apps-Mac-mini-55.local>
2016-02-04 17:49:51 -0500

    add playlist item unique in playlist.

--- app/models/playlist_item.rb
+++ app/models/playlist_item.rb
@@ -1,6 +1,7 @@
 class PlaylistItem < ActiveRecord::Base
   validates :song_id, :playlist_id, :song_ord, presence: true
   validates :song_ord, uniqueness: { scope: :playlist_id }
+  validates :song_id, uniqueness: { scope: :playlist_id }
 
   belongs_to :song
   belongs_to :playlist
