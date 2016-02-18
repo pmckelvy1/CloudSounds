43b65862611af7bf8ca0df2faa29ce84ea29a5b4
Patrick McKelvy <pmckelvy1@gmail.com>
2016-02-18 10:06:05 -0500

    switched s3 bucket pos.

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,8 @@ module JamBox
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_protocol => :https,
-      :bucket => ENV["s3_bucket"],
       :s3_credentials => {
+        :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"]
       }
